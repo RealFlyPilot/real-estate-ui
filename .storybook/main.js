ab81153084655aea9af15851ca567bd70e7f3a66
monichre <liamhellis@gmail.com>
2020-08-05 18:32:59 -0400

    moved docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,9 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@pickra/copy-code-block',
+    '@react-theming/storybook-addon',
+    'storybook-addon-jsx',
 
     {
       name: '@storybook/addon-docs',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import { configure, addDecorator, addParameters } from '@storybook/react'
 import { ThemeDecorator } from './register'
+import { jsxDecorator } from 'storybook-addon-jsx'
 
+addDecorator(jsxDecorator)
 addDecorator(ThemeDecorator)
 
 configure(require.context('../src', true, /\.stories\.tsx$/), module)
